922c6094da13b2756d862f18e6fe5a90eeef2ccc
Heyi <heta@microsoft.com>
2024-04-26 14:01:53 +0800

    Add @retrieval in _experimental

---
+++ src/promptflow-tracing/promptflow/tracing/_span_enricher.py
@@ -0,0 +1,45 @@
+from typing import Dict
+
+from .contracts.trace import TraceType
+
+
+class SpanEnricher:
+    def __init__(self):
+        pass
+
+    def enrich(self, span, inputs, output):
+        from ._trace import enrich_span_with_output
+
+        enrich_span_with_output(span, output)
+
+
+class SpanEnricherManager:
+    _instance = None
+
+    def __init__(self):
+        self._type2enricher: Dict[str, SpanEnricher] = {}
+        self._base_enricher = SpanEnricher()
+
+    @classmethod
+    def get_instance(cls) -> "SpanEnricherManager":
+        if cls._instance is None:
+            cls._instance = SpanEnricherManager()
+        return cls._instance
+
+    @classmethod
+    def register(cls, trace_type, enricher: SpanEnricher):
+        cls.get_instance()._register(trace_type, enricher)
+
+    @classmethod
+    def enrich(cls, span, inputs, output, trace_type):
+        cls.get_instance()._enrich(span, inputs, output, trace_type)
+
+    def _register(self, trace_type, enricher: SpanEnricher):
+        self._type2enricher[trace_type] = enricher
+
+    def _enrich(self, span, inputs, output, trace_type):
+        enricher = self._type2enricher.get(trace_type, self._base_enricher)
+        enricher.enrich(span, inputs, output)
+
+
+SpanEnricherManager.register(TraceType.FUNCTION, SpanEnricher())
